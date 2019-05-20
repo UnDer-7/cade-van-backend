53b215bf6ecffabd3f34475f3fb2ecdcd5d5b3bf
UnDer-7 <mateus7532@gmail.com>
2019-05-19 21:51:30 -0300

    projeto gerado

---
+++ src/main/java/com/cade/cadeonibus/CadeOnibusApplication.java
@@ -0,0 +1,13 @@
+package com.cade.cadeonibus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadeOnibusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CadeOnibusApplication.class, args);
+    }
+
+}
