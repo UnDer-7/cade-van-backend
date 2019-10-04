ce1906ffd13bab512694a92854b4d3821687a4f5
Lucas Veiga <lucasveiga09@gmail.com>
2019-10-03 22:56:30 -0300

    creating driver resource and GET endpoint

--- src/main/java/com/cade/cadeonibus/repository/DriverRepository.java
+++ src/main/java/com/cade/cadeonibus/repository/DriverRepository.java
@@ -4,7 +4,11 @@ import com.cade.cadeonibus.domain.Driver;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DriverRepository extends JpaRepository<Driver, Long> {
   Driver findByCode(final String code);
+
+  List<Driver> findAllByIdIn(List<Long> id);
 }
