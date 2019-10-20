76b6d0d88a9d741047b8595ad0a05d3e5e939abb
Lucas Veiga <lucasveiga09@gmail.com>
2019-10-20 16:12:06 -0300

    removing useless exception

--- src/main/java/com/cade/cadeonibus/rest/LocationController.java
+++ src/main/java/com/cade/cadeonibus/rest/LocationController.java
@@ -2,18 +2,16 @@ package com.cade.cadeonibus.rest;
 
 import com.cade.cadeonibus.dto.DriverLocationDTO;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-
-import java.io.IOException;
 
 @Controller
 public class LocationController {
   @MessageMapping("/location/{driverId}")
   @SendTo("/topic/location/{driverId}")
-  public DriverLocationDTO fleetLocation(@DestinationVariable final long driverId, @Payload final DriverLocationDTO payload) throws IOException {
+  public DriverLocationDTO fleetLocation(@DestinationVariable final long driverId, @Payload final DriverLocationDTO payload) {
     return payload;
   }
 }
