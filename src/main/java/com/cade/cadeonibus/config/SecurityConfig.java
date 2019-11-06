81fd07d0bcf03a5110337bc3313ddce34259489c
UnDer-7 <mateus7532@gmail.com>
2019-11-06 20:25:48 -0300

    Update

--- src/main/java/com/cade/cadeonibus/config/SecurityConfig.java
+++ src/main/java/com/cade/cadeonibus/config/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   }
 
   @Override
-  protected void configure(AuthenticationManagerBuilder auth) {
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
     auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
   }
 
